3734d429797eec086c8fda3c63ed3b285bd2ea62
Zhutian Chen <chenzhutian1992@gmail.com>
2015-01-18 00:52:31 +0800

    for rough demo

--- ManyLens/Scripts/ManyLens.js
+++ ManyLens/Scripts/ManyLens.js
@@ -392,112 +392,6 @@ var ManyLens;
     })(ManyLens.BaseD3Lens);
     ManyLens.BarChartLens = BarChartLens;
 })(ManyLens || (ManyLens = {}));
-///<reference path = "../tsScripts/D3ChartObject.ts" />
-var ManyLens;
-(function (ManyLens) {
-    var BlossomLensPane = (function (_super) {
-        __extends(BlossomLensPane, _super);
-        function BlossomLensPane(element) {
-            _super.call(this, element);
-            this._lens = new Array();
-            this._pane_radius = 100;
-            this._pane_arc = d3.svg.arc();
-            this._pane_pie = d3.layout.pie();
-            this._pane_color = d3.scale.category20();
-            this._current_pane_g = null;
-            this._pane_pie.startAngle(-Math.PI / 2).endAngle(Math.PI / 2).value(function () {
-                return 1;
-            });
-            this._pane_arc.innerRadius(this._pane_radius - 40).outerRadius(this._pane_radius);
-        }
-        BlossomLensPane.prototype.render = function () {
-            var _this = this;
-            var container = this._element;
-            container.on("click", function () {
-                _this.openPane();
-            });
-        };
-        BlossomLensPane.prototype.bindHistoryTrees = function (historyTrees) {
-            this._history_trees = historyTrees;
-            //the new tree should not be added here, need to re-write
-            this._history_trees.addTree();
-        };
-        BlossomLensPane.prototype.openPane = function () {
-            var _this = this;
-            if (this._current_pane_g && this._current_pane_g.isOpened) {
-                (function () {
-                    _this.closePane("click close");
-                })();
-            }
-            var p = d3.mouse(this._element[0][0]);
-            var timer = setTimeout(function () {
-                _this.closePane("time out close");
-            }, 2000);
-            var svg = this._element.append("g").attr("transform", "translate(" + p[0] + "," + p[1] + ")");
-            svg.selectAll("circle").data(this._pane_pie([1, 1, 1, 1, 1])).enter().append("circle").attr("class", "pane-Lens-Circle").attr("id", function (d, i) {
-                return "lens" + i;
-            }).style("fill", function (d, i) {
-                return _this._pane_color(i);
-            }).attr("r", 10).on("mouseover", function () {
-                clearTimeout(_this._current_pane_g.timer);
-            }).on("mouseout", function () {
-                _this._current_pane_g.timer = setTimeout(function () {
-                    _this.closePane("time out close");
-                }, 1000);
-            }).on("click", function (d, i) {
-                var len;
-                switch (i) {
-                    case 0:
-                        {
-                            len = new ManyLens.BarChartLens(_this._element);
-                            break;
-                        }
-                    case 1:
-                        {
-                            len = new ManyLens.LocationLens(_this._element);
-                            break;
-                        }
-                    case 2:
-                        {
-                            len = new ManyLens.NetworkLens(_this._element);
-                            break;
-                        }
-                    case 3:
-                        {
-                            len = new ManyLens.PieChartLens(_this._element);
-                            break;
-                        }
-                    case 4:
-                        {
-                            len = new ManyLens.WordCloudLens(_this._element);
-                            break;
-                        }
-                }
-                _this._lens.push(len);
-                len.render(_this._pane_color(i));
-                _this._history_trees.addNode({ color: _this._pane_color(i), lensType: len.Type, tree_id: 0 });
-                d3.event.stopPropagation();
-                _this.closePane("select a lens");
-            }).transition().duration(750).attr("transform", function (d) {
-                return "translate(" + _this._pane_arc.centroid(d) + ")";
-            });
-            this._current_pane_g = { svg_g: svg, timer: timer, isOpened: true };
-        };
-        BlossomLensPane.prototype.closePane = function (msg) {
-            console.log(msg);
-            var t = 400;
-            var closeG = this._current_pane_g;
-            clearTimeout(closeG.timer);
-            closeG.isOpened = false;
-            closeG.svg_g.selectAll(".paneCircle").transition().duration(t).attr("transform", "translate(0)").remove();
-            setTimeout(function () {
-                closeG.svg_g.remove();
-            }, t);
-        };
-        return BlossomLensPane;
-    })(ManyLens.D3ChartObject);
-    ManyLens.BlossomLensPane = BlossomLensPane;
-})(ManyLens || (ManyLens = {}));
 ///<reference path = "../tsScripts/BaseD3Lens.ts" />
 var ManyLens;
 (function (ManyLens) {
@@ -814,6 +708,112 @@ var ManyLens;
         return HistoryTrees;
     })(ManyLens.D3ChartObject);
     ManyLens.HistoryTrees = HistoryTrees;
+})(ManyLens || (ManyLens = {}));
+///<reference path = "../tsScripts/D3ChartObject.ts" />
+var ManyLens;
+(function (ManyLens) {
+    var BlossomLensPane = (function (_super) {
+        __extends(BlossomLensPane, _super);
+        function BlossomLensPane(element) {
+            _super.call(this, element);
+            this._lens = new Array();
+            this._pane_radius = 100;
+            this._pane_arc = d3.svg.arc();
+            this._pane_pie = d3.layout.pie();
+            this._pane_color = d3.scale.category20();
+            this._current_pane_g = null;
+            this._pane_pie.startAngle(-Math.PI / 2).endAngle(Math.PI / 2).value(function () {
+                return 1;
+            });
+            this._pane_arc.innerRadius(this._pane_radius - 40).outerRadius(this._pane_radius);
+        }
+        BlossomLensPane.prototype.render = function () {
+            var _this = this;
+            var container = this._element;
+            container.on("click", function () {
+                _this.openPane();
+            });
+        };
+        BlossomLensPane.prototype.bindHistoryTrees = function (historyTrees) {
+            this._history_trees = historyTrees;
+            //the new tree should not be added here, need to re-write
+            this._history_trees.addTree();
+        };
+        BlossomLensPane.prototype.openPane = function () {
+            var _this = this;
+            if (this._current_pane_g && this._current_pane_g.isOpened) {
+                (function () {
+                    _this.closePane("click close");
+                })();
+            }
+            var p = d3.mouse(this._element[0][0]);
+            var timer = setTimeout(function () {
+                _this.closePane("time out close");
+            }, 2000);
+            var svg = this._element.append("g").attr("transform", "translate(" + p[0] + "," + p[1] + ")");
+            svg.selectAll("circle").data(this._pane_pie([1, 1, 1, 1, 1])).enter().append("circle").attr("class", "pane-Lens-Circle").attr("id", function (d, i) {
+                return "lens" + i;
+            }).style("fill", function (d, i) {
+                return _this._pane_color(i);
+            }).attr("r", 10).on("mouseover", function () {
+                clearTimeout(_this._current_pane_g.timer);
+            }).on("mouseout", function () {
+                _this._current_pane_g.timer = setTimeout(function () {
+                    _this.closePane("time out close");
+                }, 1000);
+            }).on("click", function (d, i) {
+                var len;
+                switch (i) {
+                    case 0:
+                        {
+                            len = new ManyLens.BarChartLens(_this._element);
+                            break;
+                        }
+                    case 1:
+                        {
+                            len = new ManyLens.LocationLens(_this._element);
+                            break;
+                        }
+                    case 2:
+                        {
+                            len = new ManyLens.NetworkLens(_this._element);
+                            break;
+                        }
+                    case 3:
+                        {
+                            len = new ManyLens.PieChartLens(_this._element);
+                            break;
+                        }
+                    case 4:
+                        {
+                            len = new ManyLens.WordCloudLens(_this._element);
+                            break;
+                        }
+                }
+                _this._lens.push(len);
+                len.render(_this._pane_color(i));
+                _this._history_trees.addNode({ color: _this._pane_color(i), lensType: len.Type, tree_id: 0 });
+                d3.event.stopPropagation();
+                _this.closePane("select a lens");
+            }).transition().duration(750).attr("transform", function (d) {
+                return "translate(" + _this._pane_arc.centroid(d) + ")";
+            });
+            this._current_pane_g = { svg_g: svg, timer: timer, isOpened: true };
+        };
+        BlossomLensPane.prototype.closePane = function (msg) {
+            console.log(msg);
+            var t = 400;
+            var closeG = this._current_pane_g;
+            clearTimeout(closeG.timer);
+            closeG.isOpened = false;
+            closeG.svg_g.selectAll(".paneCircle").transition().duration(t).attr("transform", "translate(0)").remove();
+            setTimeout(function () {
+                closeG.svg_g.remove();
+            }, t);
+        };
+        return BlossomLensPane;
+    })(ManyLens.D3ChartObject);
+    ManyLens.BlossomLensPane = BlossomLensPane;
 })(ManyLens || (ManyLens = {}));
 ///<reference path = "../tsScripts/BaseD3Lens.ts" />
 var ManyLens;
